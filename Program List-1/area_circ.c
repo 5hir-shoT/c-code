491e503aa8082fa774880fba410cc1557740e892
5hir-shoT <shirshotarafdar@gmail.com>
2024-03-24 22:57:19 +0530

    area and circumference calculation

--- Program List-1/area_circ.c
+++ Program List-1/area_circ.c
@@ -2,7 +2,7 @@
 int main()
 {
     float pi,r,ar,ci; //variable declaration
-    printf("Steps:\n1.Enter the preffered value of pi\n 2.Enter the radius\n");
+    printf("Steps:-\n1.Enter the preffered value of pi\n 2.Enter the radius\n");
     scanf("%f%f",&pi,&r);
     ar=pi*(r*r);
     ci=2.0*pi*r;

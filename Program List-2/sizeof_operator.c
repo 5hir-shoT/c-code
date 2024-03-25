eac859b93a66c8899895a7283328d0ed3ad5d07f
5hir-shoT <shirshotarafdar@gmail.com>
2024-03-25 23:04:04 +0530

    displaying  size of every datatype using sizeof() operator

---
+++ Program List-2/sizeof_operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int x; // variable declaration.
+    printf("Enter the data=");
+    scanf("%d",&x);
+    printf("\nsize of the variable x is= %d", sizeof(x));  // Displaying the size of ?x? variable.
+    printf("\nsize of the integer data type is= %d",sizeof(int)); //Displaying the size of integer data type.
+    printf("\nsize of the character data type is= %d",sizeof(char)); //Displaying the size of character data type.
+    printf("\nsize of the floating data type is= %d",sizeof(float)); //Displaying the size of floating data type.
+    return 0;
+}

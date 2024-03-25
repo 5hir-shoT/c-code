5ca43186ca97a3ae89e2af6ae970736bfdcee98d
5hir-shoT <shirshotarafdar@gmail.com>
2024-03-25 10:03:28 +0530

    simple print

--- Program List-1/jaha_bolibo_sotto_bolibo.c
+++ Program List-1/jaha_bolibo_sotto_bolibo.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
 {
-    printf("Jaha bolibo Sotto bolibo, Sotto chhara mittha bolibo na");
+    printf("Jaha bolibo Sotto bolibo, Sotto chhara mittha bolibo na\n");
     return 0;
 }

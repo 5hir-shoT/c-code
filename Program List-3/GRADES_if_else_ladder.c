12c882eb977e0bb562fc20368c0b126e0238ff27
5hir-shoT <shirshotarafdar@gmail.com>
2024-03-27 22:35:12 +0530

    If else ladder for grade calculation

---
+++ Program List-3/GRADES_if_else_ladder.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int m;
+    printf("Input marks=");
+    scanf("%d",&m);
+    if(m>=90 && m<=100)
+    {
+        printf("Grade A+\n");
+    }
+    else if(m>=80 && m<90)
+    {
+        printf("Grade A\n");
+    }
+    else if(m>=70 && m<80)
+    {
+        printf("Grade B+\n");
+    }
+    else if(m>=60 && m<70)
+    {
+        printf("Grade B\n");
+    }
+    else if(m>=50 && m<60)
+    {
+        printf("Grade C\n");
+    }
+    else
+    {
+        printf("Grade F\n");
+    }
+    return 0;
+}

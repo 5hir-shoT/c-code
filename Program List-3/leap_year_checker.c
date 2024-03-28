cee94c1199a76b1341cd51343e35791e91c84ec6
5hir-shoT <shirshotarafdar@gmail.com>
2024-03-28 11:18:12 +0530

    Leap year checker

---
+++ Program List-3/leap_year_checker.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int y;
+    printf("Enter a year=\n");
+    scanf("%d",&y);
+    if((y%4==0) && (y%400))
+    {
+        printf("Leap year");
+    }
+    else
+    {
+        printf("Not Leap year");
+    }
+    return 0;
+}

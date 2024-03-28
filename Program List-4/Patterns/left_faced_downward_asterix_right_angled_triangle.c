735d38dab6981d1f32612465c90102977e918829
5hir-shoT <shirshotarafdar@gmail.com>
2024-03-28 11:56:01 +0530

    left faced downward asterix right angled triangle

---
+++ Program List-4/Patterns/left_faced_downward_asterix_right_angled_triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int rows;
+    printf("enter the number of rows in the pattern=\n");
+    scanf("%d",&rows);
+    for(int i=rows;i>=1;i--)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

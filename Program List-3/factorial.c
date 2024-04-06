ffb87d1596b1c44c7658973baa3df9900f5a9cea
5hir-shoT <shirshotarafdar@gmail.com>
2024-04-07 00:08:13 +0530

    factorial

--- Program List-3/factorial.c
+++ Program List-3/factorial.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,f=1,n;
+    int i,j,f=1,num;
     printf("enter a number=");
-    for(int 1)
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    f=f*i;
+    printf("The factorial of %d is=%d",num,f);
+    return 0;
 }

85ba3a6ed20b6b0483c03809d91742fa8691c25b
5hir-shoT <shirshotarafdar@gmail.com>
2024-04-07 00:14:52 +0530

    Armstrong

---
+++ Program List-3/armstrong.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,t,sum=0,r;
+    printf("enter a number=");
+    scanf("%d",&n);
+    t=n;
+    while(n>0)
+    {
+        r=n%10;
+        sum=sum+(r*r*r);
+        n=n/10;
+    }
+    if(t==sum)
+    {
+        printf("Armstrong number");
+    }
+    else
+    {
+        printf("Not Armstrong Number");
+    }
+    return 0;
+}

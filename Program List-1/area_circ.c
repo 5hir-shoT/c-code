377cb680857b9eeeb3e4935a8fde9e6f8f657bfc
5hir-shoT <shirshotarafdar@gmail.com>
2024-04-14 10:28:57 +0530

    Area and Circumference of a circle

--- Program List-1/area_circ.c
+++ Program List-1/area_circ.c
@@ -2,7 +2,7 @@
 int main()
 {
     float pi,r,ar,ci; //variable declaration
-    printf("Steps:-\n1.Enter the preffered value of pi\n 2.Enter the radius\n");
+    printf("Steps:-\n1.Enter the preffered value of pi\n2.Enter the radius\n");
     scanf("%f%f",&pi,&r);
     ar=pi*(r*r);
     ci=2.0*pi*r;
@@ -10,3 +10,16 @@ int main()
     printf("Circumference is=%f\n",ci);
     return 0;
 }
+/*
+
+output:
+
+Steps:-
+1.Enter the preffered value of pi
+2.Enter the radius
+3.14
+8
+Area is=200.960007
+Circumference is=50.240002
+
+*/

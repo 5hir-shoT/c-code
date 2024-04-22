7d6c1bb865facd9b2a9ad5ee144f6ee3bb50b002
5hir-shoT <shirshotarafdar@gmail.com>
2024-04-22 08:07:30 +0530

    roots of Quadratic equation

---
+++ class-work/roots.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main () {
+	float a,b,c,d,r1,r2,rp,ip; //rp=real part,ip=imaginary part,[r1,r2]=two roots,d=discriminant
+	printf("Enter coefficients of (a,b&c) of quadratic equation (ax^2+bx+c):");
+	scanf("%f %f %f",&a,&b,&c);
+	d=b*b-4*a*c;
+	if (d>0) {
+		r1= (-b+sqrt(d))/(2*a);
+		r2= (-b-sqrt(d))/(2*a);
+		printf("Roots are real and different:%f and %f\n",r1,r2);
+	}
+	else if (d==0)
+    {
+	    r1=r2= -b/(2*a);
+	    printf("Roots are real and same:%f and %f\n",r1,r2);
+    }
+	else if (d<0) {
+		rp=-b/(2*a);
+		ip=sqrt(-d)/(2*a);
+		printf("Roots are imaginary:%f+%fi & %f-%fi",rp,ip,rp,ip);
+	}
+	return 0;
+}

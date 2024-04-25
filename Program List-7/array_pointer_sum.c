84cf3d08cc44224ad5a0cdf22d084b2dfb675b43
5hir-shoT <shirshotarafdar@gmail.com>
2024-04-25 14:39:24 +0530

    7.1 || Array pointer sum

---
+++ Program List-7/array_pointer_sum.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int sumArray(int *arr,int size)
+{
+    int sum=0;
+    for(int i=0;i<size;i++)
+    {
+        sum+=*(arr+i);
+    }
+    return sum;
+}
+int main()
+{
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d",&size);
+    int arr[size];
+    printf("Enter %d elements of the array: \n",size);
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int totalSum=sumArray(arr,size);
+    printf("The sum of all the elements of the array is: %d\n",totalSum);
+    return 0;
+}

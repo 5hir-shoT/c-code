bdda04c41b42df63d6069fa64658a704beead047
5hir-shoT <shirshotarafdar@gmail.com>
2024-09-01 18:00:40 +0530

    Array Min vs Max

---
+++ DSA/arrayMinMax.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void findMinMax(int arr[], int n, int *min, int *max) {
+    *min = arr[0];
+    *max = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < *min) {
+            *min = arr[i];
+        }
+        if (arr[i] > *max) {
+            *max = arr[i];
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements: \n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int min, max;
+    findMinMax(arr, n, &min, &max);
+
+    printf("Minimum element: %d\n", min);
+    printf("Maximum element: %d\n", max);
+
+    return 0;
+}
+
+/*  
+    Output:
+    Enter the number of elements: 5
+    Enter the elements:
+    12
+    3
+    19
+    5
+    6
+    Minimum element: 3
+    Maximum element: 19
+*/
